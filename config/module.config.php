1dea1cc200e3500ff199c6b7210905f2e2f027e6
ThibaudGLT <thibaud.guillot@biblibre.com>
2021-10-01 09:04:23 +0000

    add french translation

--- config/module.config.php
+++ config/module.config.php
@@ -73,4 +73,14 @@ return [
             ],
         ],
     ],
+    'translator' => [
+        'translation_file_patterns' => [
+            [
+                'type' => 'gettext',
+                'base_dir' => dirname(__DIR__) . '/language',
+                'pattern' => '%s.mo',
+                'text_domain' => null,
+            ],
+        ],
+    ],
 ];
